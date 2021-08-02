8ea101e26afe03fc389f1bb507591118e7c058a3
Jeong, Kyung Hoon <niezche@hanmail.net>
2021-08-03 00:24:27 +0900

    root & footer

--- ChroZenService/ChroZenService/View/Root/View_Root.xaml.cs
+++ ChroZenService/ChroZenService/View/Root/View_Root.xaml.cs
@@ -11,33 +11,53 @@ using static ChroZenService.ChroZenService_Const;
 
 namespace ChroZenService
 {
+
     public partial class View_Root : ContentPage
     {
         public View_Root(ViewModel_Root viewModel)
         {
-
-
             InitializeComponent();
 
-            //TCPManager tcpManager = new TCPManager();
-            //EventManager.MainInitializedEvent(tcpManager);
-            //Task.Factory.StartNew(() => { tcpManager.ConnectDevice("192.168.0.88", 4242); });
-
-            //NavigationPage.SetHasNavigationBar(this, false);
-
             BindingContext = viewModel;
-         
+
+            Home.Content = Resolver.Resolve<View_Main>();
+
+            if (Footer.FindByName("HomeButton") is ImageButton home)
+            {
+                home.Clicked += OnHomeButtonClicked;
+            }
+            if (Footer.FindByName("ConfigButton") is ImageButton config)
+            {
+                config.Clicked += OnConfigButtonClicked;
+            }
+            if (Footer.FindByName("SystemButton") is ImageButton system)
+            {
+                system.Clicked += OnSystemButtonClicked;
+            }
         }
 
-        protected override void OnAppearing()
+        private void OnHomeButtonClicked(object sender, EventArgs e)
         {
+            Home.Content = Resolver.Resolve<View_Main>();
+            Home.IsVisible = true;
+            Config.IsVisible = false;
+            System.IsVisible = false;
+        }
 
+        private void OnConfigButtonClicked(object sender, EventArgs e)
+        {
+            Home.IsVisible = false;
+            Config.Content = Resolver.Resolve<View_Config>();
+            Config.IsVisible = true;
+            System.IsVisible = false;
+        }
 
-
-            //Thread t = new Thread(WaitingUntilVisibleThenRemoveSplashScreen);
-            //t.Start();
-            //WaitingUntilVisibleThenRemoveSplashScreen();
-            Console.WriteLine("==================================Waiting for view visible==================================");
+        private void OnSystemButtonClicked(object sender, EventArgs e)
+        {
+            Home.IsVisible = false;
+            Config.IsVisible = false;
+            System.Content = Resolver.Resolve<View_System>();
+            System.IsVisible = true;
         }
     }
 }
--- ChroZenService/ChroZenService/ViewModel/Root/ViewModel_Root.cs
+++ ChroZenService/ChroZenService/ViewModel/Root/ViewModel_Root.cs
@@ -4,69 +4,46 @@ using ChroZenGC.Core.Wrappers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Reflection;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace ChroZenService
 {
+
+    public static class BindingObjectExtensions
+    {
+        private static MethodInfo _bindablePropertyGetContextMethodInfo = typeof(BindableObject).GetMethod("GetContext", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static FieldInfo _bindablePropertyContextBindingFieldInfo;
+
+        public static Binding GetBinding(this BindableObject bindableObject, BindableProperty bindableProperty)
+        {
+            object bindablePropertyContext = _bindablePropertyGetContextMethodInfo.Invoke(bindableObject, new[] { bindableProperty });
+
+            if (bindablePropertyContext != null)
+            {
+                FieldInfo propertyInfo = _bindablePropertyContextBindingFieldInfo =
+                    _bindablePropertyContextBindingFieldInfo ??
+                        bindablePropertyContext.GetType().GetField("Binding");
+
+                return (Binding)propertyInfo.GetValue(bindablePropertyContext);
+            }
+
+            return null;
+        }
+    }
+
+
+
     public class ViewModel_Root : Observable
     {
-        bool timerStop = true;
-        int counter = 0;
-
-        Random random = new Random();
-
         public Model Model { get; }
 
         public ViewModel_Root(Model model)
         {
             Model = model;
-            MainView = Resolver.Resolve<View_Main>();
         }
 
-        public View MainView { get; set; }
-
-        public ICommand Home => new Command(() => MainView = Resolver.Resolve<View_Main>());
-
-        public ICommand Config => new Command(() => MainView = Resolver.Resolve<View_Config>());
-
-        public ICommand System => new Command(() => MainView = Resolver.Resolve<View_System>());
-
-        //public ICommand Config => new Command(() =>
-        //    {
-        //        if (timerStop)
-        //        {
-        //            model.Oven.Mode = OvenMode.Program;
-        //            model.Oven.Program[0].Rate = 10.0f;
-
-        //            model.State.Mode = Modes.Run;
-        //            timerStop = false;
-        //            counter = 0;
-        //            Device.StartTimer(TimeSpan.FromMilliseconds(200.0), TimerCallBack);
-        //        }
-        //    });
-
-        //private bool TimerCallBack()
-        //{
-
-        //    StateWrapper wrapper = new StateWrapper();
-        //    wrapper.Mode = model.State.Mode;
-        //    wrapper.RunTime = (float)counter / 300.0f;
-        //    wrapper.Signal[0] = (float)(random.NextDouble() * 100.0);
-        //    wrapper.Signal[1] = (float)(random.NextDouble() * 100.0 + 100.0);
-        //    wrapper.Signal[2] = (float)(random.NextDouble() * 100.0 + 200.0);
-
-
-        //    wrapper.GasSaver[0] = 1;
-        //    wrapper.GasSaver[2] = 1;
-
-        //    model.State.Binary = wrapper.Binary;
-
-        //    counter++;
-        //    return timerStop == false;
-        //}
-
-        //public ICommand System => new Command(() => { model.Oven.Mode = OvenMode.Isothermal;  timerStop = true; model.State.Mode = Modes.Ready; });
     }
 }
